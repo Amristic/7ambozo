05b7fe987cfd93406fca9a38cc42955e5e4ba82a
Amr Assem <amr.assem@onmail.com>
2024-07-08 15:52:27 +0300

    Add printHelloGit to HelloITI class

--- HelloITI.js
+++ HelloITI.js
@@ -2,7 +2,12 @@ class HelloITI {
     printHelloITI = function () {
         console.log("Hello ITI!");
     }
+
+    printHelloGit = function () {
+        console.log("Hello Git!");
+    }
 }
 
 let newITI = new HelloITI;
 newITI.printHelloITI();
+newITI.printHelloGit();
