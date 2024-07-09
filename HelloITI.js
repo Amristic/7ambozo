3e0cc8a40ac8c44bace017d660b0a0ccaf50f838
Amr Assem <amr.assem@onmail.com>
2024-07-09 13:42:05 +0300

    Add printHelloAll to HelloITI

--- HelloITI.js
+++ HelloITI.js
@@ -1,8 +1,8 @@
 class HelloITI {
     printHelloITI = () => console.log("Hello ITI!");
     printHelloGit = () => console.log("Hello Git!");
+    printHelloAll = () => console.log("Hello Mohamed, and Khalid, from Amr :D");
 }
 
 let newITI = new HelloITI;
-newITI.printHelloITI();
-newITI.printHelloGit();
+newITI.printHelloAll();
