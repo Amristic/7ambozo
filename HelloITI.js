0566fdbd135b648642076887348ff4849dbed4ac
unknown <mo.mahrous80@gmail.com>
2024-07-09 15:10:17 +0300

    say hello to gits

--- HelloITI.js
+++ HelloITI.js
@@ -1,8 +1,10 @@
 class HelloITI {
     printHelloITI = () => console.log("Hello ITI!");
     printHelloGit = () => console.log("Hello Git!");
+    printHelloGits = () => console.log("Hello Gits!");
 }
 
 let newITI = new HelloITI;
 newITI.printHelloITI();
 newITI.printHelloGit();
+newITI.printHelloGits();
