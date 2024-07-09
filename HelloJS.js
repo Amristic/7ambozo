3b58c77b567741de9f236669fbca1f6730ca9f26
unknown <mo.mahrous80@gmail.com>
2024-07-09 13:43:15 +0300

    addd HelloJSFromMo method

--- HelloJS.js
+++ HelloJS.js
@@ -1,6 +1,9 @@
 class HelloJS {
     printHelloJS = () => console.log("Hello JS!");
+    printHelloJSFromMo = () => console.log("Hello JS From Mohamed!");
 }
 
 let newJS = new HelloJS;
 newJS.printHelloJS();
+newJS.printHelloJSFromMo();
+
