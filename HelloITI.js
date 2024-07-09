9347b33c5a57e72e75ae6a6d2bffec94f85af60d
Amr Assem <amr.assem@onmail.com>
2024-07-09 14:33:17 +0300

    Conflict at line 4 HelloITI

--- HelloITI.js
+++ HelloITI.js
@@ -1,7 +1,7 @@
 class HelloITI {
     printHelloITI = () => console.log("Hello ITI from khalid!");
     printHelloGit = () => console.log("Hello Git!");
-    printHelloAll = () => console.log("Hello Mohamed, and Khalid, from Amr :D");
+    printHelloAll = () => console.log("CONFLICT TIME :D");
 }
 
 let newITI = new HelloITI;
